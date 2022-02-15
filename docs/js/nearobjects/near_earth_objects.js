2d855b62502fc13748b6e644b185b0a05d30d05f
Alexsandr <invinosanitas@yandex.ru>
2022-02-15 17:29:04 +0300

    added elements for perfect

--- docs/js/nearobjects/near_earth_objects.js
+++ docs/js/nearobjects/near_earth_objects.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let elemId = document.querySelector("#fotoDay");
 let date = {};
 let startDate = elemId.querySelector("#inputStartDate");
@@ -7,14 +9,14 @@ let putButton = elemId.querySelector("#putBattonDate");
 let header = document.createElement("tr");
 
 header.innerHTML = `<th data-type="string">Имя</th>
-  <th data-type="getsos" data-tooltip="Показать только<br>опасные объекты">Опасность</th>
+  <th data-type="getsos" data-tooltip="Показать все<br>или только<br>опасные объекты">Опасность</th>
   <th data-type="string">Тип объекта</th>
-  <th data-type="number" data-tooltip="От минимального<br>к максимальному">Max диаметр(метры)</th>
-  <th data-type="number" data-tooltip="От минимальной<br>к максимальной">Магнитуда</th>
-  <th data-type="number" data-tooltip="От минимального<br>к максимальному">Расстояние промаха(lunar)</th>
-  <th data-type="number" data-tooltip="От минимального<br>к максимальному">Расстояние промаха(Км.)</th>
-  <th data-type="number" data-tooltip="От минимальной<br>к максимальной">Скорость(Км./ч.)</th>
-  <th data-type="date" data-tooltip="От ближней<br>к дальней">Дата и время сближения</th>`;
+  <th data-type="number" data-tooltip="&#8920 Min-Max &#8921">Max диаметр(метры)</th>
+  <th data-type="number" data-tooltip="&#8920 Min-Max &#8921">Магнитуда</th>
+  <th data-type="number" data-tooltip="&#8920 Min-Max &#8921">Расстояние промаха(lunar)</th>
+  <th data-type="number" data-tooltip="&#8920 Min-Max &#8921">Расстояние промаха(Км.)</th>
+  <th data-type="number" data-tooltip="&#8920 Min-Max &#8921">Скорость(Км./ч.)</th>
+  <th data-type="date" data-tooltip="&#8920 Min-Max &#8921">Дата и время сближения</th>`;
 
 document.querySelector("tbody").append(header);
 
@@ -22,7 +24,6 @@ document.querySelector("tbody").append(header);
 putButton.addEventListener("click", function (e) {
   date.startDate = startDate.value;
   date.endDate = endDate.value;
-  // console.log(date);
 
   // Удаляем предыдущие данные
   let td = document.querySelectorAll(".table-data");
@@ -31,10 +32,8 @@ putButton.addEventListener("click", function (e) {
   }
 
   let putDataForDayFoto = new GetDataArray(4, date, 0, "", "");
-  // console.log(putDataForDayFoto);
   putDataForDayFoto.getArray();
   setTimeout(() => {
-    // console.log(putDataForDayFoto.arrayData);
     let elementCount = putDataForDayFoto.arrayData.element_count;
     let nearObjectsData = putDataForDayFoto.arrayData.near_earth_objects;
 
@@ -43,40 +42,62 @@ putButton.addEventListener("click", function (e) {
     let dengerCounter = getNearObjects(nearObjectsData);
     count.innerHTML = `Всего найдено ${elementCount} объектов. Опасных ${dengerCounter}`;
     let table = document.querySelector(".table");
-
+    let counter = 0;
     table.onclick = function (event) {
       if (event.target.tagName != "TH") return;
 
       let th = event.target;
-      // console.log(th.cellIndex, th.dataset.type);
+
       sortTable(th.cellIndex, th.dataset.type);
     };
 
     function sortTable(colNum, type) {
       let tbody = table.querySelector("tbody");
-
+      counter++;
       let rowsArray = Array.from(tbody.rows);
-      // console.log(rowsArray);
-      let compare;
 
+      let compare;
+      //Фильтруем от меньшего к большему и наоборот
       switch (type) {
         case "number":
-          compare = function (rowA, rowB) {
-            return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
-          };
+          if (counter % 1 == 0 && counter % 2 != 0) {
+            compare = function (rowA, rowB) {
+              return (
+                rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML
+              );
+            };
+          } else {
+            compare = function (rowA, rowB) {
+              return (
+                rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
+              );
+            };
+          }
+
           break;
+        case "date":
+          if (counter % 1 == 0 && counter % 2 != 0) {
+            compare = function (rowA, rowB) {
+              return (
+                new Date(rowA.cells[colNum].innerHTML) -
+                new Date(rowB.cells[colNum].innerHTML)
+              );
+            };
+          } else {
+            compare = function (rowA, rowB) {
+              return (
+                new Date(rowB.cells[colNum].innerHTML) -
+                new Date(rowA.cells[colNum].innerHTML)
+              );
+            };
+          }
+
+          break;
+        //Убираем все неопасные объекты
         case "getsos":
           compare = function (row) {
             if (row.cells[colNum].innerHTML === "~")
               return (row.hidden = !row.hidden);
-          };
-          break;
-        case "date":
-          compare = function (rowA, rowB) {
-            return (
-              new Date(rowA.cells[colNum].innerHTML) -
-              new Date(rowB.cells[colNum].innerHTML)
-            );
           };
           break;
 
