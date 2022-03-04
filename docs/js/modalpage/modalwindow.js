df1a8bd18e54c79249b0c8e6a1e4662e38c44b75
Alexsandr <invinosanitas@yandex.ru>
2022-03-04 22:36:55 +0300

    added modal pege

--- docs/js/modalpage/modalwindow.js
+++ docs/js/modalpage/modalwindow.js
@@ -2,6 +2,18 @@
 let modalInfo = {
   header: "Получить приложение",
   text: "Для того что бы получить доступ к загрузке введите свое имя и email",
+  input: `<div class="modal-input">
+            <div class="wrapper_inpt">
+                <input type="text" placeholder="Введите свое  имя">
+            </div>
+            <div class="wrapper_inpt">
+                <input type="text" placeholder="Введите свой email">
+                <div class="btn_area">
+                    ЗАГРУЗКА
+                </div>
+            </div>
+          </div>`,
+  button: "	&#10060",
 };
 let body = document.querySelector(".body");
 let clickButton = document.querySelector("#click-button");
@@ -15,19 +27,38 @@ clickButton.addEventListener("click", (e) => {
   modalWindow.className = "modal";
   modalPage.append(modalWindow);
 
-  body.setAttribute("tabindex", "-1");
   modalWindow.focus();
+  modalWindow.innerHTML = modalInfo.input;
+
+  let text = document.createElement(`h3`);
+  text.className = "modal-text";
+  text.innerHTML = modalInfo.text;
+  modalWindow.prepend(text);
 
   let header = document.createElement(`h2`);
   header.className = "modal-header";
-  modalWindow.append(header);
+  modalWindow.prepend(header);
   header.innerHTML = modalInfo.header;
 
+  let button = document.createElement(`button`);
+  button.className = "modal-batton";
+  modalWindow.prepend(button);
+  button.innerHTML = modalInfo.button;
+
   document.addEventListener("click", closeModal());
+
+  modalWindow.addEventListener("click", (e) => {
+    if (e.target.className === "modal-batton") {
+      modalPage.remove();
+      body.style.overflow = "";
+    }
+  });
+
   function closeModal() {
     document.addEventListener("click", (e) => {
       if (e.target === modalPage) {
         modalPage.remove();
+
         body.style.overflow = "";
       }
     });
@@ -35,7 +66,13 @@ clickButton.addEventListener("click", (e) => {
   document.addEventListener("keydown", function (e) {
     if (e.key === "Escape") {
       modalPage.remove();
+
       body.style.overflow = "";
+    }
+  });
+  body.addEventListener("keydown", function (e) {
+    if (e.key === "Tab") {
+      e.preventDefault();
     }
   });
 });
