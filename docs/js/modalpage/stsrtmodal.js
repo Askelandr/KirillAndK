2cbd3be4b5707c534e22dbb8379b1594080fa024
Alexsandr <invinosanitas@yandex.ru>
2022-03-09 17:11:21 +0300

    Added logics and foto

---
+++ docs/js/modalpage/stsrtmodal.js
@@ -0,0 +1,4 @@
+"use strict";
+startModalWindow(".body", "#click-button");
+startModalWindow(".body", "#click-button1");
+startModalWindow(".body", "#click-button2");
--- docs/js/modalpage/validationinput.js
+++ docs/js/modalpage/validationinput.js
@@ -1,13 +1,27 @@
 "use strict";
-// let input = document.querySelector(".modal");
-// console.log(input);
-// let inputGetName = input.querySelector("#input-get-name");
-// let errorTextInput = input.querySelector("#name-error-text");
-// let battonLoading = input.querySelector("#button-loading");
 
-// battonLoading.addEventListener("click", getErrorText());
-// function getErrorText(e) {
-//   if (e.value === "") {
-//     errorTextInput.style.opacity = 1;
-//   }
-// }
+let validInputFooter = document.querySelector("#btn-footer");
+validInputFooter.addEventListener("click", (e) => {
+  validInput("#input-footer", "#email-error-footer");
+});
+
+function validInput(selectorInput, selectorError) {
+  let inputGetEmail = document.querySelector(selectorInput);
+  let errorEmailInput = document.querySelector(selectorError);
+  let re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+  let myMail = inputGetEmail.value;
+  let valid = re.test(myMail);
+  let i = 0;
+
+  if (inputGetEmail.value === "") {
+    errorEmailInput.innerHTML = "Поле должно быть заполнено";
+    errorEmailInput.style.opacity = 1;
+  } else if (!valid) {
+    errorEmailInput.style.opacity = 0;
+    errorEmailInput.innerHTML = "Введен некорректный email";
+    errorEmailInput.style.opacity = 1;
+  } else {
+    errorEmailInput.style.opacity = 0;
+    inputGetEmail.value = "";
+  }
+}
